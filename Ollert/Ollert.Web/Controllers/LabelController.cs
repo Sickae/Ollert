e44f568c176d88a2a54a97b859cb1e396b3ea05c
sickae <sipos.bnc@gmail.com>
2019-01-30 23:53:43 +0100

    labelController left out

---
+++ Ollert/Ollert.Web/Controllers/LabelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ollert.Web.Controllers
+{
+    public class LabelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
