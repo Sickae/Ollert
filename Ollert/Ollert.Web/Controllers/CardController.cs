0446a5d54a574e1bf958b073b9b744dd8874c123
sickae <sipos.bnc@gmail.com>
2019-02-07 00:13:25 +0100

    card rename & cardList title css fix

--- Ollert/Ollert.Web/Controllers/CardController.cs
+++ Ollert/Ollert.Web/Controllers/CardController.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Ollert.Logic.Repositories;
 
 namespace Ollert.Web.Controllers
 {
     public class CardController : Controller
     {
-        public IActionResult Index()
+        private readonly CardRepository _cardRepository;
+
+        public CardController(CardRepository cardRepository)
         {
-            return View();
+            _cardRepository = cardRepository;
+        }
+
+        public IActionResult EditCardName(int id, string cardName)
+        {
+            if (!string.IsNullOrWhiteSpace(cardName) && cardName.Length > 0 && cardName.Length <= 255)
+            {
+                var card = _cardRepository.Get(id);
+                card.Name = cardName;
+                _cardRepository.Save(card);
+
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
         }
     }
 }
